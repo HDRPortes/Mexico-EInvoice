b00e7815720a0c04ba0e2c39452b58b96c8c061c
Joseph Mania <60258622+maniamartial@users.noreply.github.com>
2026-01-25 16:03:19 +0300

    update: update workspaces for mexico einvoice (#17)
    Co-authored-by: faisalepty <jojomadready@gmail.com>

---
+++ mexico_einvoice/mexico_einvoice/page/mexico_einvoice/mexico_einvoice.js
@@ -0,0 +1,7 @@
+frappe.pages['mexico-einvoice'].on_page_load = function(wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'mexico-einvoice',
+		single_column: true
+	});
+}
